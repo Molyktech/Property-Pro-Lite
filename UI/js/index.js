a3a82e94697d470d58c1743e56874a48c97ea588
Modupe Adebayo <modupeadebayo001@gmail.com>
2019-06-20 16:27:44 +0100

    feature(UI):add form for user to post property advert [Finished #166708058]

--- UI/js/index.js
+++ UI/js/index.js
@@ -4,7 +4,19 @@ const rangeSlider = document.querySelector("#range-line");
 let rangePoint = document.querySelector("#range-span");
 const tabIcons = document.querySelectorAll(".list-tab");
 const tabContent = document.querySelectorAll(".tab-text");
+const dropbox = document.getElementById("dropzone");
+const imageInput = document.querySelector("#image-input");
+const modal = document.querySelector(".modal");
+const modalTrigger = document.getElementById("modal-trigger");
+const closeButton = document.querySelector(".close-modal-button");
+const listingForm = document.querySelector("form.text-dark");
 
+modalTrigger.addEventListener("click", function(event) {
+  event.preventDefault();
+  toggleModal();
+});
+closeButton.addEventListener("click", toggleModal);
+window.addEventListener("click", clickWindow);
 const navSlider = () => {
   const hamburgerMenu = document.querySelector(".hamburger-menu");
   const nav = document.querySelector(".nav-links");
@@ -64,3 +76,103 @@ const tabSwitch = () => {
   });
 };
 tabSwitch();
+function handleFiles(files) {
+  let preview = document.getElementById("preview");
+  for (let i = 0; i < files.length; i++) {
+    const file = files[i];
+
+    if (!file.type.startsWith("image/")) {
+      continue;
+    }
+
+    const img = document.createElement("img");
+    img.classList.add("obj");
+    img.file = file;
+    preview.appendChild(img); // Assuming that "preview" is the div output where the content will be displayed.
+
+    const reader = new FileReader();
+    reader.onload = (aImg => {
+      return e => {
+        aImg.src = e.target.result;
+      };
+    })(img);
+    reader.readAsDataURL(file);
+  }
+}
+
+const dragenter = e => {
+  e.stopPropagation();
+  e.preventDefault();
+  dropbox.classList.add(" image-dropping");
+};
+
+const dragover = e => {
+  e.stopPropagation();
+  e.preventDefault();
+  dropbox.classList.add(" image-dropping");
+  evt.dataTransfer.dropEffect = "copy"; // Explicitly show this is a copy.
+};
+
+const drop = e => {
+  e.stopPropagation();
+  e.preventDefault();
+
+  const data = e.dataTransfer;
+  const files = data.files;
+
+  handleFiles(files);
+};
+
+dropbox.addEventListener("dragenter", dragenter, false);
+dropbox.addEventListener("dragover", dragover, false);
+dropbox.addEventListener("drop", drop, false);
+
+//modal for lsiting form//
+
+function toggleModal() {
+  console.log(modal);
+  //modal.style.display = "block";
+  modal.classList.toggle("show-modal");
+}
+
+function windowOnClick(event) {
+  if (event.target === modal) {
+    toggleModal();
+  }
+}
+
+/*
+
+function handleFileSelect(evt) {
+
+  var files = evt.target.files; // FileList object
+
+  // Loop through the FileList and render image files as thumbnails.
+  for (var i = 0, f; (f = files[i]); i++) {
+    // Only process image files.
+    if (!f.type.match("image.*")) {
+      continue;
+    }
+
+    var reader = new FileReader();
+
+    // Closure to capture the file information.
+    reader.onload = (function(theFile) {
+      return function(e) {
+        // Render thumbnail.
+        var span = document.createElement("span");
+        span.innerHTML = [
+          '<img class="thumb" src="',
+          e.target.result,
+          '" title="',
+          escape(theFile.name),
+          '"/>'
+        ].join("");
+        document.getElementById("previewImg").insertBefore(span, null);
+      };
+    })(f);
+
+    // Read in the image file as a data URL.
+    reader.readAsDataURL(f);
+  }
+}*/
