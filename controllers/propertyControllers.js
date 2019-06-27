1c73491a1f74c4d987ba4b20298ed0b422e1da57
Modupe Adebayo <modupeadebayo001@gmail.com>
2019-06-27 13:49:07 +0100

    feature(api):add endpoint so consumer can update property [Delivers #166871758]

--- controllers/propertyControllers.js
+++ controllers/propertyControllers.js
@@ -30,6 +30,81 @@ class Property {
       error: 'Property does not exist',
     });
   }
+
+  updateProperty(req, res) {
+    const id = parseInt(req.params.id, 10);
+    let foundProperty;
+    let propertyIndex;
+    db.map((property, index) => {
+      if (property.id === id) {
+        foundProperty = property;
+        propertyIndex = index;
+      }
+    });
+    if (!foundProperty) {
+      return res.status(404).json({
+        status: 'Error',
+        error: 'Property not found',
+
+      });
+    }
+    if (!isEmpty(req.body.status)) {
+      return res.status(400).json({
+        status: 'Error',
+        error: 'Status is required',
+      });
+    }
+    if (!isEmpty(req.body.state)) {
+      return res.status(400).json({
+        status: 'Error',
+        error: 'State is required',
+      });
+    }
+    if (!isEmpty(req.body.city)) {
+      return res.status(400).json({
+        status: 'Error',
+        error: 'City is required',
+      });
+    }
+    if (!isEmpty(req.body.address)) {
+      return res.status(400).json({
+        status: 'Error',
+        error: 'Address is required',
+      });
+    }
+    if (!isEmpty(req.body.type)) {
+      return res.status(400).json({
+        status: 'Error',
+        error: 'type is required',
+      });
+    }
+    if (!isNumber(req.body.price) || !isEmpty(req.body.price)) {
+      return res.status(400).json({
+        status: 'Error',
+        error: 'Price is required and should be a number',
+      });
+    }
+
+    const updatedProperty = {
+      id: foundProperty.id,
+      owner: req.body.owner || foundProperty.owner,
+      status: req.body.status || foundProperty.status,
+      state: req.body.state || foundProperty.state,
+      price: req.body.price || foundProperty.price,
+      city: req.body.city || foundProperty.city,
+      address: req.body.address || foundProperty.address,
+      type: req.body.type || foundProperty.type,
+      created_on: req.body.created_on || foundProperty.created_on,
+      reason: req.body.reason || foundProperty.reason,
+      description: req.body.description || foundProperty.description,
+    };
+
+    db.splice(propertyIndex, 1, updatedProperty);
+    return res.status(201).json({
+      status: 'sucsess',
+      data: updatedProperty,
+    });
+  }
 }
 
 
