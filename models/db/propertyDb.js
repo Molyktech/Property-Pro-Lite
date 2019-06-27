2497eafc579615f38eaff33c9c7ed5cd67461f3c
Modupe Adebayo <modupeadebayo001@gmail.com>
2019-06-27 13:34:37 +0100

    chore(api): add dummy data to serve as data in the db [Delivers #166871138]

---
+++ models/db/propertyDb.js
@@ -0,0 +1,30 @@
+const Properties = [{
+  id: 1,
+  owner: 'userID',
+  status: 'available',
+  state: 'Lagos',
+  price: 1000000,
+  city: 'Lagos',
+  address: 'No. 1 Admiralty way Lekki, Lagos',
+  type: '1 bedroom self-contained',
+  created_on: new Date(),
+  reason: 'Rent',
+  description: 'simple 1 bed and bath boys quartes, fenced and gated.',
+},
+{
+  id: 2,
+  owner: 'userID',
+  status: 'available',
+  state: 'Lagos',
+  price: 10000000,
+  city: 'Lagos',
+  address: 'No. 1 Admiralty way Lekki, Lagos',
+  type: '2 bedroom bungalow',
+  created_on: new Date(),
+  reason: 'Sale',
+  description: 'simple 2 bed and bath bungalow, fenced and gated.',
+},
+];
+module.exports = Properties;
+
+// export default Properties;
